23450c39e3d99c716c5ca025bc971cf2ff2d0a9f
satyamg7057 <satyamg7057>
2024-09-28 15:50:58 +0530

    fir stagged changes

---
+++ index.php
@@ -0,0 +1,218 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap link-->
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <!--font awesome -->
+    
+    <link rel="stylesheet" href="css/all.min.css">
+
+    <!--google font-->
+<link href="https://fonts.googleapis.com/css2?family=Ubuntu+Condensed&display=swap" rel="stylesheet">
+
+    <!--Custom css-->
+    <link rel="stylesheet" href="css/custom.css">
+
+    <title>Satie</title>  
+</head>
+<body>
+    <!-- Start Navigation -->
+  <nav class="navbar navbar-expand-sm navbar-dark bg-dark pl-5 fixed-top">
+    <a href="index.php" class="navbar-brand">SATIE</a>
+    <span class="navbar-text text-warning">Customer's Happiness is our Aim</span>
+    <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#myMenu">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="myMenu">
+      <ul class="navbar-nav pl-5 custom-nav">
+        <li class="nav-item"><a href="index.php" class="nav-link">Home</a></li>
+        <li class="nav-item"><a href="#Services" class="nav-link">Services</a></li>
+        <li class="nav-item"><a href="#registration" class="nav-link">Registration</a></li>
+        <li class="nav-item"><a href="Requester/RequesterLogin.php" class="nav-link">Login</a></li>
+        <li class="nav-item"><a href="#Contact" class="nav-link">Contact</a></li>
+      </ul>
+    </div>
+  </nav> <!-- End Navigation -->
+
+  <!-- Start Header Jumbotron-->
+  <header class="jumbotron back-image" style="background-image: url(images/Banner1.jpeg);">
+    <div class="myclass mainHeading text-center ">
+      <h1 class="text-uppercase text-warning font-weight-bold">Welcome to SATIE</h1>
+      <p class=" text-white font-italic">Its all about your Comfort</p>
+      <a href="Requester/RequesterLogin.php" class="btn btn-dark mr-4">Login</a>
+      <a href="#registration" class="btn btn-dark mr-4 ">Sign Up</a>
+    </div>
+  </header> <!-- End Header Jumbotron -->
+
+  <div class="container">
+    <!--Introduction Section-->
+    <div class="jumbotron">
+      <h3 class="text-center ">Satie Services</h3>
+      <p>
+        SATIE Services is India’s leading chain of multi-brand Electronics and Electrical service
+        workshops offering
+        wide array of services. We focus on enhancing your uses experience by offering world-class
+        Electronic
+        Appliances maintenance services. Our sole mission is “To provide Electronic Appliances care
+        services to
+        keep the devices fit and healthy and customers happy and smiling”.
+
+        With well-equipped Electronic Appliances service centres and fully trained mechanics, we
+        provide quality
+        services with excellent packages that are designed to offer you great savings.
+
+        Our state-of-art workshops are conveniently located in many cities across the country. Now you
+        can book
+        your service online by doing Registration.
+      </p>
+
+    </div>
+  </div>
+  <!--Introduction Section End-->
+  <!-- Start Services -->
+  <div class="container text-center border-bottom" id="Services">
+    <h2>Our Services</h2>
+    <div class="row mt-4">
+      <div class="col-sm-4 mb-4">
+        <a href="#"><i class="fas fa-tv fa-8x text-warning"></i></a>
+        <h4 class="mt-4">Electronic Appliances</h4>
+      </div>
+      <div class="col-sm-4 mb-4">
+        <a href="#"><i class="fas fa-sliders-h fa-8x text-info"></i></a>
+        <h4 class="mt-4">Preventive Maintenance</h4>
+      </div>
+      <div class="col-sm-4 mb-4">
+        <a href="#"><i class="fas fa-cogs fa-8x text-danger"></i></a>
+        <h4 class="mt-4">Fault Repair</h4>
+      </div>
+    </div>
+  </div> <!-- End Services -->
+
+  <!-- Start Registration  -->
+  <?php include('userRegistration.php') ?>
+  <!-- End Registration  -->
+
+  <!-- Start Happy Customer  -->
+  <div class="jumbotron bg-dark" id="Customer">
+    <!-- Start Happy Customer Jumbotron -->
+    <div class="container">
+      <!-- Start Customer Container -->
+      <h2 class="text-center text-white">Happy Customers</h2>
+      <div class="row mt-5">
+        <div class="col-lg-3 col-sm-6">
+          <!-- Start Customer 1st Column-->
+          <div class="card shadow-lg mb-2">
+            <div class="card-body text-center">
+              <img src="images/avtar1.jpeg" class="img-fluid" style="border-radius: 100px;">
+              <h4 class="card-title">Angelina Gupta</h4>
+              <p class="card-text"> Great service, efficient communication and a really easy way to get a mortgage with lots of help and support to get the right deal.</p>
+            </div>
+          </div>
+        </div> <!-- End Customer 1st Column-->
+
+        <div class="col-lg-3 col-sm-6">
+          <!-- Start Customer 2nd Column-->
+          <div class="card shadow-lg mb-2">
+            <div class="card-body text-center">
+              <img src="images/avtar2.jpeg" class="img-fluid" style="border-radius: 100px;">
+              <h4 class="card-title">Sonu Sharma</h4>
+              <p class="card-text">Best customer service. I really appreciate all the efforts by (SATIE). From customer service department he put on solving my issue. Great work!</p>
+            </div>
+          </div>
+        </div> <!-- End Customer 2nd Column-->
+
+        <div class="col-lg-3 col-sm-6">
+          <!-- Start Customer 3rd Column-->
+          <div class="card shadow-lg mb-2">
+            <div class="card-body text-center">
+              <img src="images/avtar3.jpeg" class="img-fluid" style="border-radius: 100px;">
+              <h4 class="card-title">Chaaru cat</h4>
+              <p class="card-text">A fantastic organisation!  The team are really informed and go the extra mile at every stage. I would recommend them unreservedly.</p>
+            </div>
+          </div>
+        </div> <!-- End Customer 3rd Column-->
+
+        <div class="col-lg-3 col-sm-6">
+          <!-- Start Customer 4th Column-->
+          <div class="card shadow-lg mb-2">
+            <div class="card-body text-center">
+              <img src="images/avtar4.jpeg" class="img-fluid" style="border-radius: 100px;">
+              <h4 class="card-title">Lalla Don</h4>
+              <p class="card-text"> I chatted with Satie. They answered all my questions. They had my interest at heart and found the best coverage at a good price.</p>
+            </div>
+          </div>
+        </div> <!-- End Customer 4th Column-->
+      </div> <!-- End Customer Row-->
+    </div> <!-- End Customer Container -->
+  </div> <!-- End Customer Jumbotron -->
+
+  <!--Start Contact Us-->
+  <div class="container" id="Contact">
+    <!--Start Contact Us Container-->
+    <h2 class="text-center mb-4">Contact Us</h2> <!-- Contact Us Heading -->
+    <div class="row">
+
+      <!--Start Contact Us Row-->
+      <?php include('contactform.php'); ?>
+      <!-- End Contact Us 1st Column -->
+
+      <div class="col-md-4 text-center">
+        <!-- Start Contact Us 2nd Column-->
+        <strong>Headquarter:</strong> <br>
+        SATIE Pvt Ltd, <br>
+        Ward-02, Burhar <br>
+        Shahdol - 484110 <br>
+        Phone: +91 7057037531 <br>
+        <a href="#" target="_blank">www.satie.com</a> <br>
+
+        <br><br>
+        <strong>Anuppur Branch:</strong> <br>
+        SATIE Pvt Ltd, <br>
+        Hospital colony, Apr <br>
+        MP - 482002 <br>
+        Phone: +91 8319279810 <br>
+        <a href="#" target="_blank">www.satie.com</a> <br>
+      </div> <!-- End Contact Us 2nd Column-->
+    </div> <!-- End Contact Us Row-->
+  </div> <!-- End Contact Us Container-->
+  <!-- End Contact Us -->
+
+  <!-- Start Footer-->
+  <footer class="container-fluid bg-dark text-warning mt-5" style="border-top: 3px solid #DC3545;">
+    <div class="container">
+      <!-- Start Footer Container -->
+      <div class="row py-3">
+        <!-- Start Footer Row -->
+        <div class="col-md-6">
+          <!-- Start Footer 1st Column -->
+          <span class="pr-2">Follow Us: </span>
+          <a href="https://www.facebook.com/eti.tiwari.129" target="_blank" class="pr-2 fi-color"><i class="fab fa-facebook-f"></i></a>
+          <a href="#" target="_blank" class="pr-2 fi-color"><i class="fab fa-twitter"></i></a>
+          <a href="https://www.instagram.com/independent_melvin_._/" target="_blank" class="pr-2 fi-color"><i class="fab fa-instagram"></i></a>
+          <a href="https://www.youtube.com/" target="_blank" class="pr-2 fi-color"><i class="fab fa-youtube"></i></a>
+         
+        </div> <!-- End Footer 1st Column -->
+
+        <div class="col-md-6 text-right">
+          <!-- Start Footer 2nd Column -->
+          <small> Designed by Satyam & Ananya &copy; 2022.
+          </small>
+          <small class="ml-2"><a href="Admin/login.php">Admin Login</a></small>
+        </div> <!-- End Footer 2nd Column -->
+      </div> <!-- End Footer Row -->
+    </div> <!-- End Footer Container -->
+  </footer> <!-- End Footer -->
+
+  <!-- Boostrap JavaScript -->
+  <script src="js/jquery.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/all.min.js"></script>
+</body>
+</html>
